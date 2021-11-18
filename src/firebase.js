ac6d81e4d3771edf92ab8d276245493037f04678
RoshniJH <80098774+RoshniJH@users.noreply.github.com>
2021-11-18 16:38:31 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA76gQDtPBAu_Oke2XhArjoIvVgfdduLlE",
+    authDomain: "netflix-clone-947eb.firebaseapp.com",
+    projectId: "netflix-clone-947eb",
+    storageBucket: "netflix-clone-947eb.appspot.com",
+    messagingSenderId: "826507489293",
+    appId: "1:826507489293:web:d49b68f7dac044c968a544"
+  };
