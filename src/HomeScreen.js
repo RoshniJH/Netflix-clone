9c0ef74ce66e346dcf9cdbfb953e3fd75390ed2a
RoshniJH <80098774+RoshniJH@users.noreply.github.com>
2021-12-21 13:35:14 +0530

    implemented Rows
    Rows implementation for Home screen

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import './HomeScreen.css';
 import Nav from './Nav';
+import Row from './Row';
 import Banner from './Banner';
+import requests from './Requests';
 function HomeScreen() {
     return (
         <div className="homeScreen">
@@ -11,7 +13,14 @@ function HomeScreen() {
 
              <Banner />
 
-            {/* rows */}
+            <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+            <Row title="Trending" fetchUrl={requests.fetchTrending}/>
+            <Row title="Top Rater" fetchUrl={requests.fetchTopRated}/>
+            <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+            <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+            <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+            <Row title="Romance Movies" fetchUrl={requests.fetchRomanticMovies}/>
+            <Row title="Documentaries" fetchUrl={requests.fetchDocumetaries}/>
         </div>
     )
 }
