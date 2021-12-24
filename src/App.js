c53cb5c1558dc742e347d0ba8e15a563c153f032
RoshniJH <jahirrose488@gmail.com>
2021-12-24 14:36:26 +0530

    Redux implementation

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import HomeScreen  from './Screens/HomeScreen';
 import LoginScreen from './Screens/LoginScreen';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {auth} from "./firebase";
+import { login, logout, selectUser } from './features/userSlice';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 function App() {
-  const user = null;
-
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
   useEffect(() => {
     const unsubscribe =  auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
         //logged in 
-        console.log(userAuth);
+       // console.log(userAuth);
+        dispatch(login({
+          uid:userAuth.uid,
+          email:userAuth.email,
+        }));
       } else {
         //logged out
+        dispatch(logout);
       }
     })
     return unsubscribe;
@@ -27,7 +35,7 @@ function App() {
         <LoginScreen />
       ): (
         <Routes> {/*V6 upgrade switch -> Routes*/}
-           
+       
             <Route exact path="/" element={ <HomeScreen/>} />
 
         </Routes>
